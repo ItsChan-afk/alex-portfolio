886d95432aa256cb3b6624cfb87d254881f3f7c9
Mac <chanminswe26@gmail.com>
2024-08-10 22:48:50 +0630

    footer fixed :)

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,18 @@ import React from 'react';
 import NavBar from './Pages/features/nav-bar/NavBar';
 import Footer from './Pages/features/footer/Footer';
 import './App.css'; // Make sure to import your CSS file
-import TwoCarousel from './Pages/commission/my-carousels/TwoCarousel';
-import articleData from '/public/resources/article.json';
-import carouselData from '/public/resources/commisions.json';
-import OneCarousel from './Pages/commission/my-carousels/OneCarousel';
+
 const App = () => {
   return (
     <div className="app-container">
       <NavBar />
+      <br />
+      <main>
 
+      </main>
+      <footer>
         <Footer />
-
+      </footer>
     </div>
 
   );
